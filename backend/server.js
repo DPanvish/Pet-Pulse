5778766904336507d038bdca91756946ffaf8d6b
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-05-30 17:39:25 +0530

    CORS Updated

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
