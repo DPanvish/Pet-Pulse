89147e0152072bc1145c9a9cf6e4625d435e8073
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-05-30 17:20:20 +0530

    Backend URL Updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.MODE === 'production' ? import.meta.env.BASE_URL : 'http://localhost:5000/api',
+    withCredentials: true,
 });
 
 // Automatically attach the token to every request
