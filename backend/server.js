25124d55684edb3ef860603646ab7c9d06784384
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-06-01 23:18:40 +0530

    frontend cors updated

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 const allowedOrigins = [
-    'https://pet-pulse-delta.vercel.app', 
+    'https://pet-pulse-phi.vercel.app', 
     'http://localhost:5173',              
     'http://localhost:3000'               
 ];
